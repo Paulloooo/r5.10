06d096bfa09d9419c36b3f1fa5a2405b45d6669e
Paulloooo <115004751+Paulloooo@users.noreply.github.com>
2023-10-26 14:32:59 +0200

    Add java script

---
+++ java/src/main/java/ad_cassandra/App.java
@@ -0,0 +1,61 @@
+package ad_cassandra;
+
+import java.io.File;
+import java.net.InetSocketAddress;
+import java.time.Duration;
+import java.util.Arrays;
+
+import com.datastax.oss.driver.api.core.CqlSession;
+import com.datastax.oss.driver.api.core.config.DriverConfigLoader;
+import com.datastax.oss.driver.api.core.cql.ResultSet;
+import com.datastax.oss.driver.api.core.cql.SimpleStatement;
+import com.datastax.oss.driver.api.core.cql.Statement;
+
+public class App {
+
+    public static void main(String[] args) {
+        
+        String keyspaceName = "ad_demo1";
+        String tableName = "ad_javatable";
+
+        
+        try (CqlSession session = CqlSession.builder()
+        		.addContactPoint(new InetSocketAddress("127.0.0.1", 9042))
+        		.withLocalDatacenter("Mars")
+                .build()) {
+
+            
+            session.execute(String.format("CREATE KEYSPACE IF NOT EXISTS %s WITH "
+                    + "replication = {'class': 'SimpleStrategy', 'replication_factor': 1}", keyspaceName));
+
+            
+            session.execute(String.format("USE %s", keyspaceName));
+
+            
+            Statement<?> createTableStatement = SimpleStatement.builder(
+                    String.format("CREATE TABLE IF NOT EXISTS %s (id UUID PRIMARY KEY, name TEXT, age INT)", tableName))
+                    .setTimeout(Duration.ofSeconds(5))
+                    .build();
+
+            session.execute(createTableStatement);
+
+            
+            session.execute(String.format("TRUNCATE TABLE %s", tableName));
+
+           
+            session.execute(String.format("INSERT INTO %s (id, name, age) VALUES (uuid(), 'Albert', 39)", tableName));
+            session.execute(String.format("INSERT INTO %s (id, name, age) VALUES (uuid(), 'Bernard', 37)", tableName));
+            session.execute(String.format("INSERT INTO %s (id, name, age) VALUES (uuid(), 'Carles', 32)", tableName));
+
+            
+            ResultSet resultSet = session.execute(String.format("SELECT * FROM %s", tableName));
+            resultSet.forEach(row -> {
+                System.out.printf("ID: %s, Name: %s, Age: %d\n",
+                        row.getUuid("id"), row.getString("name"), row.getInt("age"));
+            });
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+}
